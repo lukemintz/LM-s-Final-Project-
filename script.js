450783dac4a86261f1142df195170df1cc9d0faa
lukemintz <lukesebastianmintz@gmail.com>
2017-04-17 21:05:18 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,200 @@
+function SportsSelect() {
+    var x = document.getElementById("SportsSelectFunction").value;
+
+
+switch(x) {
+    case "AmericanFootball":
+    document.getElementById("SportsTextArea").value = "There are 17 openly LGBT American Football players. Michael Sam Jr, 27, who played for the St Louis Rams, attracted international headlines when he came out as gay in 2013";
+    break;
+
+	case "Baseball":
+    document.getElementById("SportsTextArea").value = "There are 6 openly LGBT baseball players.  Dave Pallone, aged 64, umpired in the American Baseball league during the 1980s, but only felt able to come out as gay in 1998. He has campaigned for LGBT equality ever since.";
+    break;
+	
+	case "Basketball":
+    document.getElementById("SportsTextArea").value = "There are 19 openly LGBT basketball players. Elena Delle Donne, 27, who plays for the Washington Mystics, came out as a lesbian in Vogue magazine in 2016, announcing her engagement to her girlfriend.";
+    break;
+	
+	case "BeachVolleyball":
+    document.getElementById("SportsTextArea").value = "There are 4 openly LGBT beach volleyball players. Liliane Maestrini, who played for Brazil at the 2013 World Championships, married a fellow team player in 2013.";
+    break;
+	
+	case "Bodybuilding":
+    document.getElementById("SportsTextArea").value = "There are 5 openly LGBT bodybuilders. American Rebekka Armstrong has campaigned for HIV rights since 1992.";
+    break;
+	
+	case "Boxing":
+    document.getElementById("SportsTextArea").value = "There are 8 openly LGBT boxers. Nicola Adams won gold for Britain at the 2012 London Olympics.";
+    break;
+	
+	case "Cricket":
+    document.getElementById("SportsTextArea").value = "There are 4 openly LGBT cricketers. Steven Davies, an English left-handed batsmen who played for Somerset, came out in an interview with The Daily Telegraph in in 2011.";
+    break;
+	
+	case "Cycling":
+    document.getElementById("SportsTextArea").value = "There are 8 openly LGBT cyclists. English racing cyclist Emma Trott is the younger sister of Olympic gold medallist Laura Trott.";
+    break;
+	
+	case "Diving":
+    document.getElementById("SportsTextArea").value = "There are 6 openly LGBT divers. British Olympic medallist Tom Daley first competed in the Beijing Olympics, aged only 14. He is now engaged to Hollywood director and Oscar-winner Dustin Lance Black.";
+    break;
+	
+	case "Equestrian":
+    document.getElementById("SportsTextArea").value = "There are 6 openly LGBT horse racers. Hans Peter Minderhoud represented the Netherlands in the dressage event at the 2008 Beijing Olympics. His partner is teammate Edward Gal. ";
+    break;
+	
+	case "FieldHockey":
+    document.getElementById("SportsTextArea").value = "There are 11 openly LGBT field hockey players. Australian Gus Johnston played as a professional field hockey goalkeeper before campaigning against homophobia in sport.";
+    break;
+	
+	case "FigureSkating":
+    document.getElementById("SportsTextArea").value = "There are 25 openly LGBT figure skaters. Johnny Weir won bronze at the 2008 World Championships. In 2013 he attracted criticism from other LGBT activists when he refused to boycott the Winter Olympic Games in Sochi.";
+    break;
+	
+	case "Football":
+    document.getElementById("SportsTextArea").value = "There are 47 openly LGBT football players. German midfielder Thomas Hitzlberger. He attracted international attention when he became the highest-profile footballer to come out as gay in 2014. He has since spoken publically about the extent of homophobia in German football, saying that some of his teammates refused to shower with him when he first came out. In the UK, There are 0 openly LGBT male players in the Premier League, but several in the Women’s League.";
+    break;
+	
+	case "Golf":
+    document.getElementById("SportsTextArea").value = "There are 6 openly LGBT sports players. American golfer Rosie Jones won 13 LPGA career victories during the 1980s and 1990s, and in 2004 was one of the first golfers to come out as a lesbian.";
+    break;
+	
+	case "Gymnastics":
+    document.getElementById("SportsTextArea").value = "There are 5 openly LGBT gymnasts. Canadian Kris Burley won four national championships over the 1990s, and now campaigns against homophobia in sport.";
+    break;
+	
+	case "IceHockey":
+    document.getElementById("SportsTextArea").value = "There are 12 openly LGBT ice hockey players. Canadian Brendan Burke, who plays for the RedHawks, campaigns prominently against homophobia in college sports.";
+    break;
+	
+	case "MotorRacing":
+    document.getElementById("SportsTextArea").value = "There are 6 openly LGBT motor racers. American Stephen Rhodes became the first openly gay racer to compete in the NASCAR World Championships in 2003.";
+    break;
+	
+	case "Rowing":
+    document.getElementById("SportsTextArea").value = "There are 3 openly LGBT rowers. Marnie McBean, Canadian three-time Olympic gold medallist, came out as a lesbian after retiring from professional sport, and married her partner in 2014.";
+    break;
+	
+	case "Rugby":
+    document.getElementById("SportsTextArea").value = "There are 6 openly LGBT rugby players. Gareth Thomas represented Wales, and with 100 test matches was the most capped rugby union player in Wales until 2011. He came out as gay in 2009, and the following year received Stonewall’s Hero of the Year Award.";
+    break;
+	
+	case "SpeedSkating":
+    document.getElementById("SportsTextArea").value = "There are 7 openly LGBT speed skaters. Sanne van Kerkhof represented the Netherlands at the 2010 Winter Olympics.";
+    break;
+	
+	case "Swimming":
+    document.getElementById("SportsTextArea").value = "There are 11 openly LGBT swimmers. Australian five-time Olympic gold medallist Ian Thorpe came out in 2014 in a television interview with Michael Parkinson, after years media speculation.";
+    break;
+	
+	case "Tennis":
+    document.getElementById("SportsTextArea").value = "There are 14 openly LGBT tennis players. Amelie Mauresmo won two Grand Slam singles titles for France at Wimbledon and the Australian Open, and was formerly World Number 1. She was one of the first professional tennis players to come out as a lesbian in 1999, and in 2015 announced she was having a baby with her child.";
+    break;
+	
+	case "TrackandField":
+    document.getElementById("SportsTextArea").value = "There are 10 openly LGBT track and field athletes. Robert Newton was for many years the British Number 1 hurdling athlete. He was the only openly gay British athlete to compete at the 2004 Athens Olympics.";
+    break;
+	
+	case "Wrestling":
+    document.getElementById("SportsTextArea").value = "There are 8 openly LGBT wrestlers. Canadian professional wrestler Milena Leticia Roucka, known under WWE ring name as Rose Mendes, came out as bisexual in 2014.";
+    break;
+     
+}
+}
+
+/*---------------------------------------------------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------------------------------------------*/
+/*---------------------------------------------------------------------------------------------------------------------------*/
+
+function CountrySelect() {
+    var x = document.getElementById("CountrySelectFunction").value;
+
+
+switch(x) {
+    case "Australia":
+    document.getElementById("CountryTextArea").value = "Australia has 20 professional sportspeople who are openly LGBT, including 5-time Olympic gold medal swimmer Ian Thorpe.";
+    break;
+
+	case "Austria":
+    document.getElementById("CountryTextArea").value = "Austria has 3 professional sportspeople who are openly LGBT.";
+    break;
+	
+	case "Belgium":
+    document.getElementById("CountryTextArea").value = "Belgium has 2 professional sportspeople who are openly LGBT.";
+    break;
+	
+	case "Brazil":
+    document.getElementById("CountryTextArea").value = "Brazil has 4 professional sportspeople who are openly LGBT";
+    break;
+	
+	case "Canada":
+    document.getElementById("CountryTextArea").value = "Canada has 36 professional sportspeople who are openly LGBT.";
+    break;
+	
+	case "CzechRepublic":
+    document.getElementById("CountryTextArea").value = "The Czech Republic has 2 professional sportspeople who are openly LGBT.";
+    break;
+	
+	case "France":
+    document.getElementById("CountryTextArea").value = "France has 3 professional sportspeople who are openly LGBT."
+    break;
+	
+	case "Germany":
+    document.getElementById("CountryTextArea").value = "Germany has 16 professional sportspeople who are openly LGBT.";
+    break;
+	
+	case "Holland":
+    document.getElementById("CountryTextArea").value = "Holland has 18 professional sportspeople who are openly LGBT.";
+    break;
+	
+	case "Ireland":
+    document.getElementById("CountryTextArea").value = "Ireland has 1 professional sportsperson who is openly LGBT.";
+    break;
+	
+	case "Italy":
+    document.getElementById("CountryTextArea").value = "Italy has 1 professional sportsperson who is openly LGBT.";
+    break;
+	
+	case "Mexico":
+    document.getElementById("CountryTextArea").value = "Mexico has 1 professional sportsperson who is openly LGBT.";
+    break;
+	
+	case "NewZealand":
+    document.getElementById("CountryTextArea").value = "New Zealand has 5 professional sportspeople who are openly LGBT.";
+    break;
+	
+	case "Norway":
+    document.getElementById("CountryTextArea").value = "Norway has 9 professional sportspeople who are openly LGBT.";
+    break;
+	
+	case "PuertoRico":
+    document.getElementById("CountryTextArea").value = "Puerto Rico has 2 professional sportspeople who are openly LGBT.";
+    break;
+	
+	case "Samoa":
+    document.getElementById("CountryTextArea").value = "Samoa has 1 professional sportsperson who is openly LGBT.";
+    break;
+	
+	case "SouthAfrica":
+    document.getElementById("CountryTextArea").value = "South Africa has 2 professional sportspeople who are openly LGBT.";
+    break;
+	
+	case "Sweden":
+    document.getElementById("CountryTextArea").value = "Sweden has 16 professional sportspeople who are openly LGBT.";
+    break;
+	
+	case "Thailand":
+    document.getElementById("CountryTextArea").value = "Samoa has 1 professional sportsperson who is openly LGBT.";
+    break;
+	
+	case "UnitedKingdom":
+    document.getElementById("CountryTextArea").value = "The United Kingdom has 26 professional sportspeople who are openly LGBT.";
+    break;
+	
+	case "UnitedStates":
+    document.getElementById("CountryTextArea").value = "The United States has 150 professional sportspeople who are openly LGBT.";
+    break;
+     
+}
+}
